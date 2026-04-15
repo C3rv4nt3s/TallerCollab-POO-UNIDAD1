788a9adb911f9c6e3f4e6fe4b3aa757a3459bae4
Alexander Cervantes <alexandercervantes0413@gmail.com>
2026-04-14 22:49:59 -0500

    feat(taller3): actualizacion del archivo

--- src/taller3/Main.java
+++ src/taller3/Main.java
@@ -4,10 +4,24 @@
  */
 package taller3;
 
-/**
- *
- * @author Acer
- */
 public class Main {
-    
+    public static void main(String[] args) {
+
+        // COCHES
+        Coche c1 = new Coche("Toyota", "Corolla");
+        Coche c2 = new Coche("Mazda", "3");
+
+        c1.mostrarCoche();
+        c2.mostrarCoche();
+
+        Coche.mostrarContador();
+
+        System.out.println("----------------");
+
+        // MATEMATICAS
+        System.out.println("Suma: " + Matematicas.suma(5, 3));
+        System.out.println("Resta: " + Matematicas.resta(10, 4));
+        System.out.println("Multiplicación: " + Matematicas.multiplicacion(2, 6));
+        System.out.println("División: " + Matematicas.division(10, 2));
+    }
 }
